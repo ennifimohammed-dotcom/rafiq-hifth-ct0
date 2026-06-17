9147fb0602393e7c81711ebd6308186583e51b75
ennifimohammed-dotcom <ennifimohammed@gmail.com>
2026-06-17 14:47:37 +0100

    feat: lib/domain/repositories/auth_repository.dart

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -3,5 +3,6 @@ abstract class AuthRepository {
   Stream<String?> watchTeacherId();
   String? get currentTeacherId;
   Future<void> signIn({required String email, required String password});
+  Future<void> register({required String email, required String password});
   Future<void> signOut();
 }
